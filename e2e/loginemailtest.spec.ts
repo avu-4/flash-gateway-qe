8ca49294288a905c8806a2b5638d45034e2cb5a8
Yasmina <yasmina.abrahams26@icloud.com>
2026-09-01 08:59:21 +0200

    Added merchant navigtion test

---
+++ e2e/loginemailtest.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('FlashGateway', async ({ page }) => {
+  await page.goto('http://localhost:4173');
+    await page.getByRole('link', { name: 'Sign In' }).first().click();
+
+ // Expect a title "to contain" a substring.
+ await expect(page).toHaveURL('http://localhost:4173/login')
+  await page.getByRole('textbox', { name: 'Email address' }).fill('minniemeeee13@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Password123!');
+ await page.getByRole('button', { name: 'Sign In' }).click();
+await expect(page.getByRole('heading', { name: 'Access your account' })).toBeVisible({timeout: 30000});
+});
+
+/*importing a object consiisting of test function and expect funtion from the module or component called @playwright/test. */
---
+++ login.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('FlashGateway', async ({ page }) => {
+  await page.goto('http://localhost:4173');
+    await page.getByRole('link', { name: 'Sign In' }).first().click();
+
+ 
+ await expect(page).toHaveURL('http://localhost:4173/login')
+  await page.getByRole('textbox', { name: 'Email address' }).fill('merchant@flashgateway.local');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Password123!');
+ await page.getByRole('button', { name: 'Sign In' }).click();
+await expect(page.getByRole('heading', { name: 'Portfolio Overview' })).toBeVisible({timeout: 30000});
+});
+
